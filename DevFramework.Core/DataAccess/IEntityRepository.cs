fb064c062dada2243907f1c50a41b216cc5326b6
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-20 23:59:50 +0300

    IEntityRepository Arayüzünün Oluşturulması

--- DevFramework.Core/DataAccess/IEntityRepository.cs
+++ DevFramework.Core/DataAccess/IEntityRepository.cs
@@ -8,7 +8,7 @@ using DevFramework.Core.Entities;
 
 namespace DevFramework.Core.DataAccess
 {
-    public interface IEntityRepository<T>where  T:class,IEntity,new()
+    public interface IEntityRepository<T>where  T:class,IEntity,new() 
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
         T Get(Expression<Func<T, bool>> filter);
