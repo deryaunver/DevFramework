56e925d0f7a5b62315da03ebe3f0e0f87b35d6c8
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-24 01:10:38 +0300

    Entity Framework İçin Birim Testinin Yazılması

---
+++ DevFramework.Northwind.DataAccess.Tests/EntityFramework/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DevFramework.Northwind.DataAccess.Concrete.EntityFramework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevFramework.Northwind.DataAccess.Tests.EntityFramework
+{
+    [TestClass]
+    public class EntityFrameworkTests
+    {
+        [TestMethod]
+        public void Get_all_returns_all_products()
+        {
+            EfProductDal productDal= new EfProductDal();
+            var result = productDal.GetAll();
+            Assert.AreEqual(81,result.Count);
+        }
+        public void Get_all_with_parameter_returns_filtered_products()
+        {
+            EfProductDal productDal = new EfProductDal();
+            var result = productDal.GetAll(p=>p.ProductName.Contains("ab"));
+            Assert.AreEqual(4, result.Count);
+        }
+    }
+}
