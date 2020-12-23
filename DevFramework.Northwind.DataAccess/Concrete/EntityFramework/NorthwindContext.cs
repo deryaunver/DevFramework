290fff2cc4e5c5e130c6c80b24ea19a3ed7791ab
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-23 23:35:41 +0300

    Context İmplementasyonunun Yapılması

---
+++ DevFramework.Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Northwind.Entities.Concrete;
+
+namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework
+{
+   public class NorthwindContext:DbContext
+    {
+        public NorthwindContext()
+        {
+            //Hazır bir veritabanı kullanıyorum .Veritabanımı Tasarladım.
+            //O yüzdem sen gidip veritabanında( görmediğğinde onu oluşturmaya çalışma) birşey oluşturma.
+            Database.SetInitializer<NorthwindContext>(null);
+        }
+        public DbSet<Product>Products { get; set; }
+    }
+}
