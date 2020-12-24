59ffedb53438be474585563d72ba89bb1b781817
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-24 23:21:13 +0300

    Validation İçin CrossCuttingConcerns Yapılandırması

---
+++ DevFramework.Core/CrossCuttingConcerns/Validation/FluentValidation/ValidatorTool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace DevFramework.Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+    public class ValidatorTool
+    {
+        //IValidator: buraya productValidator gelebilri,categoryValidator Gelebilir .
+        //Bunların herbirinin aslında Base i :IValidator dür.
+        //T entity:tüm nesnelerin basei , product,category...
+        public static void Validate<T>(IValidator<T> validator, T entity)
+        {
+
+            var result = validator.Validate(entity);
+            if (result.Errors.Count > 0)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
