6b09f2acd4482d879b48d88482db8581f56fd7bb
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-24 22:08:27 +0300

    İş Katmanının İmplementasyonu

---
+++ DevFramework.Northwind.Business/Concrete/Manager/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Northwind.Business.Abstract;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.Concrete;
+
+namespace DevFramework.Northwind.Business.Concrete.Manager
+{
+  public  class ProductManager:IProductService
+  {
+      private IProductDal _productDal;
+
+      public ProductManager(IProductDal productDal)
+      {
+          _productDal = productDal;
+      }
+
+      public List<Product> GetAll()
+      {
+          return _productDal.GetAll();
+      }
+
+        public Product Get(int id)
+        {
+            return _productDal.Get(P => P.ProductID == id);
+        }
+
+        public Product Add(Product product)
+        {
+            return _productDal.Add(product);
+        }
+    }
+}
