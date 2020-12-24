5d3ca07a54b438a77b6dc1540b82656219e1e85e
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-24 16:46:20 +0300

    Entity Framework'te Complex Tiplerle Çalışmak

--- DevFramework.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DevFramework.Northwind.DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -5,11 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 using DevFramework.Core.DataAccess.EntityFramewrok;
 using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.Entities.ComplexTypes;
 using DevFramework.Northwind.Entities.Concrete;
 
 namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework
 {
    public class EfProductDal:EfEntityRepositoryBase<Product,NorthwindContext>,IProductDal
     {
+        public List<ProductDetail> GetProductDetails()
+        {
+            using (NorthwindContext context= new NorthwindContext())
+            {
+                var result = from p in context.Products
+                    join c in context.Categories on p.CategoryID equals c.CategoryId
+                    select new ProductDetail
+                    {
+                        ProductId = p.ProductID,
+                        ProductName = p.ProductName,
+                        CategoryName = c.CategoryName
+                    };
+                return result.ToList();
+            }
+            
+        }
     }
 }
