eeca148ba1ad359032cb9811559e0f17596b7ec2
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2020-12-30 17:27:40 +0300

    Hataların Yakalanması ve Loglanması

--- DevFramework.Northwind.Business/Properties/AssemblyInfo.cs
+++ DevFramework.Northwind.Business/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using DevFramework.Core.Aspects.Postsharp.ExceptionAspects;
 using DevFramework.Core.Aspects.Postsharp.LogAspects;
 using DevFramework.Core.CrossCuttingConcerns.Logging.Log4Net.Loggers;
 
@@ -16,6 +17,7 @@ using DevFramework.Core.CrossCuttingConcerns.Logging.Log4Net.Loggers;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: LogAspect(typeof(DatabaseLogger), AttributeTargetTypes = "DevFramework.Northwind.Business.Concrete.Manager.*")]
+[assembly: ExceptionLogAspect(typeof(DatabaseLogger), AttributeTargetTypes = "DevFramework.Northwind.Business.Concrete.Manager.*")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
