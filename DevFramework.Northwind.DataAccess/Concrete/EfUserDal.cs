9c86de2162bd1957e7c5ec83d16e4d493b34f4ce
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-01-02 14:31:18 +0300

    User Nesnesinin Veri Erişim İmplementasyonu

---
+++ DevFramework.Northwind.DataAccess/Concrete/EfUserDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Core.DataAccess.EntityFramewrok;
+using DevFramework.Northwind.DataAccess.Abstract;
+using DevFramework.Northwind.DataAccess.Concrete.EntityFramework;
+using DevFramework.Northwind.Entities.Concrete;
+
+namespace DevFramework.Northwind.DataAccess.Concrete
+{
+   public class EfUserDal:EfEntityRepositoryBase<User,NorthwindContext>,IUserDal
+    {
+    }
+}
--- DevFramework.Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DevFramework.Northwind.DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -19,6 +19,7 @@ namespace DevFramework.Northwind.DataAccess.Concrete.EntityFramework
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
         //yazdığımız mappingi ayağa kaldırıyoruz.
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
