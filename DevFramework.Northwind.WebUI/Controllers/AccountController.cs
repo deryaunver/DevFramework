3e49879b437ab38fc49858057a0e75b52bc72c40
derya ünver <yl1930138003@ogr.sdu.edu.tr>
2021-01-02 13:36:57 +0300

    PostAuthenticateRequest Operasyonu ile Identity İmplementasyonu

---
+++ DevFramework.Northwind.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DevFramework.Core.CrossCuttingConcerns.Security.Web;
+
+namespace DevFramework.Northwind.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public string Login()
+        {
+            AuthenticationHelper.CreateAuthCookie(new Guid(),"deryaunver","dryunver94qgmail.com",DateTime.Now.AddDays(15),new []{"Admin"},false,"Derya","Ünver"  );
+            return "User is authenticated";
+        }
+    }
+}
--- DevFramework.Northwind.WebUI/Global.asax.cs
+++ DevFramework.Northwind.WebUI/Global.asax.cs
@@ -1,9 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Security;
+using DevFramework.Core.CrossCuttingConcerns.Security.Web;
 using DevFramework.Core.Utilities.Mvc.Infrastructure;
 using DevFramework.Northwind.Business.DependencyResolvers.Ninject;
 
@@ -17,6 +21,47 @@ namespace DevFramework.Northwind.WebUI
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
             ControllerBuilder.Current.SetControllerFactory(new NinjectControllerFactory(new BusinessModule()));
+        }
+        //asp.net mvc de kullanýcý bilgiilerine eriþim ...
+        public override void Init()
+        {
+            //bu eventi luþturcak ortam 
+            PostAuthenticateRequest += MvcApplication_PostAuthenticateRequest;
+            base.Init(); 
+        }
+
+        private void MvcApplication_PostAuthenticateRequest(object sender, EventArgs e)
+        {
+            //kiþinin  bilgilerine eriþilebilir olduðu zamana karþýlýk gelir.
+            //adým 1 : Cookie ye eriþmeye çalýþ bir cooki olluþtur.
+
+            try
+            {
+                var authCookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+                //kýsaca biz bu isimle bir cooki oluþturcaðýmýzý söylemiþ olduk.
+                if (authCookie == null)
+                {
+                    return;
+                }
+
+                var encTicket = authCookie.Value;
+                if (string.IsNullOrEmpty(encTicket))
+                {
+                    return;
+                }
+
+                var ticket = FormsAuthentication.Decrypt(encTicket);
+                var securityUtilities = new SecurityUtilities();
+                var identiy = securityUtilities.FormsAuthTicketToIdentity(ticket);
+                var principal = new GenericPrincipal(identiy, identiy.Roles);
+                HttpContext.Current.User = principal;
+                Thread.CurrentPrincipal = principal;//backend de kullansýn diye
+            }
+            catch (Exception )
+            {
+          
+            }
+          
         }
     }
 }
